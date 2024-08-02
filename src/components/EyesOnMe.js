644b3f7e72829bb35f5e2f42b0c332c03b288001
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-02 11:44:41 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function eventFocus() {
+    console.log("Good!");
+  }
+  function eventBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <button onFocus={eventFocus} onBlur={eventBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
